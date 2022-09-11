2dca4d6d6cf6196ba2f77adb19c09b3b3d0cc08f
Saul Spatz <saul.spatz@gmail.com>
2022-09-11 10:23:43 -0500

    Update Makefile for rank symmetries

--- scripts/makeMake.py
+++ scripts/makeMake.py
@@ -7,6 +7,17 @@ from partitions import partitions
 with open("../build/includes.txt", 'w') as fout:
     for p in partitions(25, 4, 13):
         match p:
+            case (s,h,d,c) if s > h > d > c:
+                text =  f'dist{s}{h}{d}{c}.o:'
+                text += f'../include/rank{s}.h '
+                text += f'../include/suit{h}.h '
+                text += f'../include/suit{d}.h '
+                text += f'../include/suit{c}.h \\\n'
+                text += f'    ../include/swords{s}.h '
+                text += f'    ../include/hearts{h}.h '
+                text += f'    ../include/diamonds{d}.h '
+                text += f'    ../include/clubs{c}.h \\\n'
+                text += f'    ../include/types.h\n'
             case (s,h,d,c):
                 text =  f'dist{s}{h}{d}{c}.o:'
                 text += f'../include/suit{s}.h '
@@ -18,6 +29,15 @@ with open("../build/includes.txt", 'w') as fout:
                 text += f'    ../include/diamonds{d}.h '
                 text += f'    ../include/clubs{c}.h \\\n'
                 text += f'    ../include/types.h\n'
+            case (s,h,d) if s > h >d:
+                text =  f'dist{s}{h}{d}.o: '
+                text += f'../include/ranks{s}.h '
+                text += f'../include/suit{h}.h '
+                text += f'../include/suit{d}.h \\\n'
+                text += f'   ../include/swords{s}.h '
+                text += f'   ../include/hearts{h}.h '
+                text += f'   ../include/diamonds{d}.h '
+                text += f'   ../include/types.h\n'
             case (s,h,d):
                 text =  f'dist{s}{h}{d}.o: '
                 text += f'../include/suit{s}.h '
@@ -29,9 +49,9 @@ with open("../build/includes.txt", 'w') as fout:
                 text += f'   ../include/types.h\n'
             case (s,h):
                 text =  f'dist{s}{h}.o: '
-                text += f'../include/suit{s}.h '
+                text += f'../include/ranks{s}.h '
                 text += f'../include/suit{h}.h '
-                text += f'../include/spades{s}.h '
+                text += f'../include/swords{s}.h '
                 text += f'../include/hearts{h}.h\\\n'
                 text += '    ../include/types.h\n'
             case _:
